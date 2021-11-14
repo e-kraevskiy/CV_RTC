c95fcd9ec0e5d37517f2a9c26fa557d7c71bf1b4
Eugeniy Krarvskiy <krai41@yandex.ru>
2021-11-14 11:25:26 +0300

    Добавляет тележку

--- 1LAB/main.cpp
+++ 1LAB/main.cpp
@@ -14,9 +14,9 @@ using namespace cv;
 
 const string IMG_PATH = "/home/eugeniy/CV_1.jpg";
 
-float getCorrectedCos (int x, int scale) {
+float getCorrectedCos (int x, int scale, int offset = 0) {
     float tmp = cos(float(x)/scale)*scale;
-    return tmp;
+    return tmp + float(offset);
 }
 
 int main(int argc, char *argv[])
@@ -27,17 +27,10 @@ int main(int argc, char *argv[])
     Mat img;
     string path = IMG_PATH;
     img = imread(path, IMREAD_COLOR);
-
-//    line(img, (60, 20), (400, 200), (0, 0, 255), 5);
-//    line(img, Point(60, 20), Point(400, 200), Scalar(0, 0, 255), 5);
-//    circle(img, Point( 300, 300 ), 32.0, Scalar( 0, 0, 255 ), 20);
-//    circle(img, Point( 200, 200 ), 32.0, Scalar( 0, 0, 255 ), -1, 8 );
-
     int sin_center = img.size().height/2;
 
     //Рисуем синусоиду
     for (int t = 0; t <= img.size().width; t++) {
-//        circle(img, Point( t, (cos(((float)t)/100)*100)+100 ), 1.0, Scalar( 255, 255, 255 ), -1);
         circle(img, Point( t, getCorrectedCos(t, 100)+sin_center), 4.0, Scalar( 255, 255, 255 ), -1);
     }
 
@@ -46,11 +39,17 @@ int main(int argc, char *argv[])
 
     //Сдесь должна отрисовываться тележка
     for (int t = 0; t <= img.size().width; t++) {
-        circle(img, Point( t, getCorrectedCos(t, 100)+sin_center), 2.0, Scalar( 0, 0, 255 ), -1);
-        imshow("Display Window",img);
-        waitKey(1);
-    }
+        Mat tmpImg = img.clone();
+//        rectangle(,)
+        rectangle(tmpImg, Point( t-15, getCorrectedCos(t, 100, -10)+sin_center),
+                  Point( t+15, getCorrectedCos(t, 100, 10)+sin_center), Scalar( 0, 0, 255 ), -1);
+//        circle(tmpImg, Point( t, getCorrectedCos(t, 100)+sin_center), 10.0, Scalar( 0, 0, 255 ), -1);
+        imshow("Display Window",tmpImg);
 
+        if (waitKey(1) == 27)
+            break;
+
+    }
 
     return app.exec();
 }
