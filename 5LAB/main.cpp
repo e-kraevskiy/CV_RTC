8df12f1f397e610f4d19cc5799b75d22e6162105
Eugeniy Kraevskiy <krai41@yandex.ru>
2022-04-16 13:23:14 +0300

    Оформил калибровку камеры

--- 5LAB/main.cpp
+++ 5LAB/main.cpp
@@ -17,7 +17,7 @@ void createBoard() {
     cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
     cv::Ptr<cv::aruco::CharucoBoard> board = cv::aruco::CharucoBoard::create(5, 7, 0.04f, 0.02f, dictionary);
     cv::Mat boardImage;
-    board->draw(cv::Size(600, 500), boardImage, 10, 1);
+    board->draw(cv::Size(1500, 2100), boardImage, 10, 1);
     cv::imwrite("BoardImage.jpg", boardImage);
 }
 
